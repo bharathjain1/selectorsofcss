432c2aae9f5eeff35b3513de2bbfb1ecb50f509e
bharathjain <bharathjain@bharath-desktop>
2020-12-12 16:33:14 +0530

    selectors css

---
+++ selectors/src/selectors/selectorss.java
@@ -0,0 +1,45 @@
+package selectors;
+import java.util.*;
+
+public class selectorss {
+
+	public static void main(String[] args) {
+	System.out.println("publicatioo]n date");
+	System.out.println(".form-item-field-data-de-publicacao-value-max:before, .form-item-field-data-licitacao-value-max:before,\n"
+			+ ".form-item-field-periodo-inscricao-final-value:before {\n"
+			+ "    position: absolute;\n"
+			+ "    margin-top: 8px;\n"
+			+ "    font-weight: bold;\ncontent: \"De\";\n"
+			+ "    position: absolute;\n"
+			+ "    margin-top: 31px;\n"
+			+ "    font-weight: boldmargin-left: 25pxmargin-left: 10px;margin-left: 10px;padding-top: 40px;;"
+			+
+			
+			"");
+	
+	System.out.println("object");
+	
+	System.out.println(" .item_ar_objeto ::first-letter {text-transform: uppercase;");
+	
+	System.out.println("Binding date");
+	System.out.println(".form-item-title-licitacao input::placeholder {\n"
+			+ "  	font-size: 10px;\n"
+			+ "  	font-family: Arial;\n"
+			+ "\n"
+			+ "\n"
+			+ ".item_ar_licitacao{\n"
+			+ "      background: #C1E0B7;\n"
+			+ "      margin-bottom: 10px;\n"
+			+ "      margin-top: 15px;\n"
+			+ "      display: block;\n"
+			+ "      padding: 5px;\n"
+			+ "      float: left;\n"
+			+ "      width: 100%;\n"
+			+ "\n"
+			+ "}\n"
+			+ "\n"
+			+ "");
+	
+	}
+
+}
